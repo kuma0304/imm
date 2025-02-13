a421d9bcff2b8baa66a4bb41ed6f435766c8b8a3
webbyrk <webbyrk@gmail.com>
2025-02-12 23:23:15 -0500

    Header Setup

---
+++ assets/js/custom/theme.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+
+function mobileNavigationOn() {
+    body.classList.add('mobile-menu-active');
+}
+
+function mobileNavigationOff() {
+    body.classList.remove('mobile-menu-active');
+}
