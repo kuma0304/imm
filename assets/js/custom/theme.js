3256f3eff91bc38653ea7646453149365f9e9878
webbyrk <webbyrk@gmail.com>
2025-02-13 14:55:46 -0500

    Header, Banner, Company

--- assets/js/custom/theme.js
+++ assets/js/custom/theme.js
@@ -1,9 +1,25 @@
+// Mobile Navigation
 const body = document.querySelector('body');
 
 function mobileNavigationOn() {
     body.classList.add('mobile-menu-active');
 }
-
 function mobileNavigationOff() {
     body.classList.remove('mobile-menu-active');
 }
+
+// Owl Carousel
+$('.hero-owl').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
+    responsive:{
+        0:{
+            items:1
+        },
+    }
+})
