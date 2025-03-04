05c91048bb21ff4dd000282f7ff76762900ad15a
webbyrk <webbyrk@gmail.com>
2025-03-03 19:26:59 -0500

    About and careers

--- assets/js/custom/theme.js
+++ assets/js/custom/theme.js
@@ -44,6 +44,96 @@ $('.testimonials-owl').owlCarousel({
     ]
 });
 
+// About Course Owl Carousel
+$('.about-coursel-owl').owlCarousel({
+    loop: false,
+    margin: 25,
+    nav: true,
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768 : {
+            items: 2
+        },
+        1200 : {
+            items: 3
+        },
+    },
+    navText: [
+        '<span class="material-symbols-outlined">arrow_left_alt</span>', 
+        '<span class="material-symbols-outlined">arrow_right_alt</span>'
+    ]
+});
+
+// Professor Owl Carousel
+$('.prof-coursel-owl').owlCarousel({
+    loop: false,
+    margin: 25,
+    nav: true,
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768 : {
+            items: 2
+        },
+        1200 : {
+            items: 4
+        },
+    },
+    navText: [
+        '<span class="material-symbols-outlined">arrow_left_alt</span>', 
+        '<span class="material-symbols-outlined">arrow_right_alt</span>'
+    ]
+});
+
+function equalizeOwlHeights() {
+    var maxHeight = 0;
+    
+    // Reset heights before recalculating
+    $(".prof-coursel-owl .item .theme-box").css("height", "auto");
+
+    // Find the tallest div
+    $(".prof-coursel-owl .item .theme-box").each(function () {
+        var thisHeight = $(this).outerHeight();
+        if (thisHeight > maxHeight) {
+            maxHeight = thisHeight;
+        }
+    });
+
+    // Apply the max height to all divs
+    $(".prof-coursel-owl .item .theme-box").css("height", maxHeight + "px");
+}
+
+// About Gallery Carousel
+$('.about-gallery-owl').owlCarousel({
+    loop: false,
+    margin: 25,
+    nav: true,
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+    },
+    navText: [
+        '<span class="material-symbols-outlined">arrow_left_alt</span>', 
+        '<span class="material-symbols-outlined">arrow_right_alt</span>'
+    ]
+});
+
 // Custom Accordion and Tabs
 $(document).ready(function() {
     // Handle accordion header clicks
